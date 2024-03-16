465549081054f9df016efa5767e4c33daae4bcaa
RenanTheFato <138338709+RenanTheFato@users.noreply.github.com>
2024-03-16 19:19:54 -0300

    Add files via upload

--- js/loadDashboardData.js
+++ js/loadDashboardData.js
@@ -11,13 +11,38 @@ window.onload = function() {
         codigoAcesso: codigoAcesso
     };
 
-    const mainDashboard = document.getElementById('mainDashboard');
-    const html = `
-        <h1>Bem-vindo(a), ${cargo}!</h1>
-        <p>CPF: ${userInfo.cpf}</p>
-        <p>Cargo: ${userInfo.cargo}</p>
-        <p>Código de Acesso: ${userInfo.codigoAcesso}</p>
+    const dashboardRole = document.getElementById('dashboardRole');
+    const roleHtml = `<h1>Bem-vindo(a), ${cargo}!</h1> `;
+    
+    dashboardRole.innerHTML = roleHtml;
 
-    `;
-    mainDashboard.innerHTML = html;
+    const dashboardCPF = document.getElementById('dashboardCPF');
+    const CPFHtml = `<p>CPF: ${userInfo.cpf}</p> `;
+    
+    dashboardCPF.innerHTML = CPFHtml;
+
+    const dashboardRole2 = document.getElementById('dashboardRole2');
+    const dashboardRole2Html = `<p>Cargo: ${userInfo.cargo}</p>`;
+    
+    dashboardRole2.innerHTML = dashboardRole2Html;
+
+    const dashboardCode = document.getElementById('dashboardCode');
+    const CodeHtml = `<p>Código de Acesso: ${userInfo.codigoAcesso}</p> `;
+    
+    dashboardCode.innerHTML = CodeHtml;
+
+
+//Styles
+const dashboardTab = document.getElementsByClassName("dashboardTab")[0];
+if (cargoUsuario == "Gerente") {
+    dashboardTab.style.backgroundColor = "#21222A";
+} else if (cargoUsuario == "Fornecedor") {
+    dashboardTab.style.backgroundColor = "#306569";
+} else if (cargoUsuario == "Func-Admin") {
+    dashboardTab.style.backgroundColor = "#3b7db3";
+} else if (cargoUsuario == "Func-Caixa") {
+    dashboardTab.style.backgroundColor = "#8ed2ec";
+}
+
+
 };
